10ab03decceaab239bfb4119ab0d93e31ab5b940
maasa <maasa.giss2@gmail.com>
2025-12-08 10:44:37 +0900

    pnpm up major updates

--- eslint.config.ts
+++ eslint.config.ts
@@ -149,6 +149,9 @@ const config: Linter.Config[] = [
       'react/prop-types': 'off',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'error',
+      // Disable new strict rules from react-hooks v5
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/immutability': 'off',
     },
     settings: {
       react: {
